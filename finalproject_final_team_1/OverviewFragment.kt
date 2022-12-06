a843b6eeae73a94027eaeca6e39623408d29fcc3
robpaud <113536098+robpaud@users.noreply.github.com>
2022-12-06 13:27:25 -0800

    Update OverviewFragment.kt

--- finalproject_final_team_1/OverviewFragment.kt
+++ finalproject_final_team_1/OverviewFragment.kt
@@ -2,7 +2,15 @@ package com.example.finalproject_final_team_1
 
 import android.graphics.Color
 import android.os.Bundle
+import android.annotation.SuppressLint
+import android.content.Intent
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.text.SimpleDateFormat
 import androidx.fragment.app.Fragment
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +18,7 @@ import android.widget.Toast
 import com.androidplot.xy.*
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 //The test data we use for the chart
 var week1 = arrayOf<Number>(12,10,5,8,0,3,0);
@@ -27,17 +31,9 @@ var week3 = arrayOf<Number>(1,6,4,0,3,0,8);
  * create an instance of this fragment.
  */
 class OverviewFragment : Fragment(R.layout.fragment_overview) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,26 +41,6 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OverviewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OverviewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -101,7 +77,27 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
         renderer.setBarOrientation(BarRenderer.BarOrientation.SIDE_BY_SIDE)
         renderer.setBarGroupWidth(BarRenderer.BarGroupWidthMode.FIXED_GAP,75f)
 
-    }
+        //code for google calendar integration
+        val mButton = requireView().findViewById<FloatingActionButton>(R.id.fButton)
+        val startTime = "2022-02-1T09:00:00"
+        val endTime = "2022-02-1T12:00:00"
+        // Parsing the date and time
+        val mSimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val mStartTime = mSimpleDateFormat.parse(startTime)
+        val mEndTime = mSimpleDateFormat.parse(endTime)
+        //Intent is started to create an event with given time
+        mButton.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_EDIT)
+            mIntent.type = "vnd.android.cursor.item/event"
+            mIntent.putExtra("beginTime", mStartTime.time)
+            mIntent.putExtra("time", true)
+            mIntent.putExtra("rule", "FREQ=YEARLY")
+            mIntent.putExtra("endTime", mEndTime.time)
+            mIntent.putExtra("title", "CS481 Event")
+            startActivity(mIntent)
+        } //end of mButton.setOnClickListener
+
+    } //end of onViewCreated
 
     private fun fillArrays()
     {
@@ -111,12 +107,12 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
                 week1[document.data.getValue("day") as Int] = document.data.getValue("hours") as Int
             }
         }
-         dbReference = database.collection("data").whereEqualTo("week", 2).get().addOnSuccessListener {
+        dbReference = database.collection("data").whereEqualTo("week", 2).get().addOnSuccessListener {
             for(document in it ){
                 week1[document.data.getValue("day") as Int] = document.data.getValue("hours") as Int
             }
         }
-         dbReference = database.collection("data").whereEqualTo("week", 3).get().addOnSuccessListener {
+        dbReference = database.collection("data").whereEqualTo("week", 3).get().addOnSuccessListener {
             for(document in it ){
                 week1[document.data.getValue("day") as Int] = document.data.getValue("hours") as Int
             }
