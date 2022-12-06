a47f5c040b39df70d275545165072c92375d9ed9
adam-salinggih <113716769+adam-salinggih@users.noreply.github.com>
2022-12-05 18:49:59 -0800

    Add files via upload

--- app/src/main/java/com/example/finalproject_final_team_1/AddTaskFragment.kt
+++ app/src/main/java/com/example/finalproject_final_team_1/AddTaskFragment.kt
@@ -5,30 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddTaskFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AddTaskFragment : Fragment(R.layout.fragment_add_task) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +18,28 @@ class AddTaskFragment : Fragment(R.layout.fragment_add_task) {
         return inflater.inflate(R.layout.fragment_add_task, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddTaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddTaskFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    /** == DATABASE ENTRY CREATION ==
+     * @param email email used.
+     * @param password account password
+     *
+     *
+     * @Note Please lowercase all email entry before passed as a parameter
+     * @Note Add entries as needed
+     */
+    fun addTask(email: String, password: String){
+        val user : MutableMap<String, Any> = HashMap()
+        val db = FirebaseFirestore.getInstance()
+
+        //Add additional fields as needed
+        user["email"] = email
+        user["password"] = password
+
+        db.collection("users").add(user).addOnFailureListener{
+                e ->
+            Toast.makeText(this.context, "Unable to add entry", Toast.LENGTH_LONG).show()
+        }.addOnSuccessListener {
+            Toast.makeText(this.context, "Entry has been added to the database", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
--- app/src/main/java/com/example/finalproject_final_team_1/MainActivity.kt
+++ app/src/main/java/com/example/finalproject_final_team_1/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.mainMenuLoginButton).setOnClickListener {
-            startActivity(Intent(this, AppOverview::class.java))
+            if(login("johndoe@email.com", "123")) { //Testing login
+                startActivity(Intent(this, AppOverview::class.java))
+            }
         }
 
+    }
+
+    /** == DATABASE AUTHENTICATION ==
+     * @param email email used.
+     * @param password account password
+     *
+     * @return true if authentication is successful
+     *
+     * @Note Please lowercase all email entry before passed as a parameter
+     */
+    private fun login(email: String, password: String) : Boolean{
+        var returnValue = false
+        val database = FirebaseFirestore.getInstance()
+
+        //Check the database and match all email
+        database.collection("data").whereEqualTo("email", email).get().addOnSuccessListener {
+            database.collection("data").whereEqualTo("password", password).get().addOnSuccessListener {
+                returnValue = true
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        //default return
+        Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+        return returnValue
     }
 }
